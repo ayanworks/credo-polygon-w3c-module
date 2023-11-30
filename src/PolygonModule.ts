42cfad64b4db7b17f03f0cc95e554d48fad003ee
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-11-30 17:45:32 +0530

    refactor: remove polygon sdk module (#2)
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/PolygonModule.ts
+++ src/PolygonModule.ts
@@ -1,25 +1,21 @@
 import type { PolygonModuleConfigOptions } from './PolygonModuleConfig'
 import type { DependencyManager, Module } from '@aries-framework/core'
 
-import { AgentConfig } from '@aries-framework/core'
-
 import { PolygonModuleConfig } from './PolygonModuleConfig'
 import { PolygonLedgerService } from './ledger'
-import { PolygonSDK } from './sdk/PolygonSdk'
 
 export class PolygonModule implements Module {
+  public readonly config: PolygonModuleConfig
+
+  public constructor(options: PolygonModuleConfigOptions) {
+    this.config = new PolygonModuleConfig(options)
+  }
+
   public register(dependencyManager: DependencyManager) {
     // Warn about experimental module
-    dependencyManager
-      .resolve(AgentConfig)
-      .logger.warn(
-        "The '@aries-framework/polygon' module is experimental and could have unexpected breaking changes. When using this module, make sure to use strict versions for all @aries-framework packages."
-      )
+    dependencyManager.registerInstance(PolygonModuleConfig, this.config)
 
     // Services
     dependencyManager.registerSingleton(PolygonLedgerService)
-
-    // SDK
-    dependencyManager.registerSingleton(PolygonSDK)
   }
 }

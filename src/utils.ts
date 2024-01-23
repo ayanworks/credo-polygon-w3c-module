3376cfb676a0294497905a345d5e55fbe2530b27
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-01-23 13:43:31 +0530

    fix: remove duplicate file
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-import { PolygonDID } from '@ayanworks/polygon-did-registrar'
-
-export const generateSecp256k1KeyPair = async () => {
-  const { privateKey, publicKeyBase58, address } = await PolygonDID.createKeyPair()
-  return { privateKey, publicKeyBase58, address }
-}

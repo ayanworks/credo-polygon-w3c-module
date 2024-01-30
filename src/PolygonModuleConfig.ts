853d9bb464f20a95761c7524ce2729846daea313
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-01-30 17:52:07 +0530

    refactor: use signing key (#13)
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/PolygonModuleConfig.ts
+++ src/PolygonModuleConfig.ts
@@ -1,12 +1,9 @@
-import type { Buffer } from '@aries-framework/core'
-
 /**
  * PolygonModuleConfigOptions defines the interface for the options of the PolygonModuleConfig class.
  */
 export interface PolygonModuleConfigOptions {
   rpcUrl?: string
   didContractAddress?: string
-  privateKey?: Buffer
   fileServerToken?: string
   schemaManagerContractAddress?: string
   serverUrl?: string
@@ -15,7 +12,6 @@ export interface PolygonModuleConfigOptions {
 export class PolygonModuleConfig {
   public readonly rpcUrl: string | undefined
   public readonly didContractAddress: string | undefined
-  public readonly privateKey: Buffer | undefined
   public readonly fileServerToken: string | undefined
   public readonly schemaManagerContractAddress: string | undefined
   public readonly serverUrl: string | undefined
@@ -23,14 +19,12 @@ export class PolygonModuleConfig {
   public constructor({
     didContractAddress,
     fileServerToken,
-    privateKey,
     rpcUrl,
     schemaManagerContractAddress,
     serverUrl,
   }: PolygonModuleConfigOptions) {
     this.rpcUrl = rpcUrl
     this.didContractAddress = didContractAddress
-    this.privateKey = privateKey
     this.fileServerToken = fileServerToken
     this.schemaManagerContractAddress = schemaManagerContractAddress
     this.serverUrl = serverUrl
--- src/ledger/PolygonLedgerService.ts
+++ src/ledger/PolygonLedgerService.ts
@@ -1,8 +1,10 @@
-import type { AgentContext } from '@aries-framework/core'
+import type { AgentContext, Wallet } from '@aries-framework/core'
 
-import { AriesFrameworkError, injectable } from '@aries-framework/core'
+import { AskarWallet } from '@aries-framework/askar'
+import { AriesFrameworkError, DidRepository, WalletError, injectable } from '@aries-framework/core'
 import { PolygonDID } from '@ayanworks/polygon-did-registrar'
 import { PolygonSchema } from '@ayanworks/polygon-schema-manager'
+import { SigningKey } from 'ethers'
 
 import { PolygonModuleConfig } from '../PolygonModuleConfig'
 
@@ -17,45 +19,43 @@ interface SchemaRegistryConfig {
 
 @injectable()
 export class PolygonLedgerService {
-  private schemaRegistry: PolygonSchema | undefined
   private rpcUrl: string | undefined
-  private contractAddress: string | undefined
+  private didContractAddress: string | undefined
+  private schemaManagerContractAddress: string | undefined
+  private fileServerToken: string | undefined
+  private fileServerUrl: string | undefined
 
   public constructor({
     didContractAddress,
-    privateKey,
     rpcUrl,
     fileServerToken,
     schemaManagerContractAddress,
     serverUrl,
   }: PolygonModuleConfig) {
     this.rpcUrl = rpcUrl
-    this.contractAddress = didContractAddress
-
-    if (schemaManagerContractAddress && privateKey && rpcUrl && fileServerToken && serverUrl && didContractAddress) {
-      this.schemaRegistry = new PolygonSchema({
-        didRegistrarContractAddress: didContractAddress,
-        rpcUrl,
-        fileServerToken,
-        privateKey: privateKey.toString('hex'),
-        schemaManagerContractAddress,
-        serverUrl,
-      })
-    }
+    this.didContractAddress = didContractAddress
+    this.schemaManagerContractAddress = schemaManagerContractAddress
+    this.fileServerToken = fileServerToken
+    this.fileServerUrl = serverUrl
   }
 
   public async createSchema(
     agentContext: AgentContext,
     { did, schemaName, schema }: { did: string; schemaName: string; schema: object }
   ) {
-    agentContext.config.logger.info(`Creating schema on ledger: ${did}`)
+    const publicKeyBase58 = await this.getPublicKeyFromDid(agentContext, did)
 
-    if (!this.schemaRegistry) {
-      agentContext.config.logger.error(`Schema registry not found`)
-      throw new AriesFrameworkError(`Schema registry not configured. Please check your configuration`)
+    if (!publicKeyBase58) {
+      throw new AriesFrameworkError('Public Key not found in wallet')
     }
 
-    const response = await this.schemaRegistry.createSchema(did, schemaName, schema)
+    const signingKey = await this.getSigningKey(agentContext.wallet, publicKeyBase58)
+
+    const schemaRegistry = this.createSchemaRegistryInstance(signingKey)
+
+    agentContext.config.logger.info(`Creating schema on ledger: ${did}`)
+
+    const response = await schemaRegistry.createSchema(did, schemaName, schema)
     if (!response) {
       agentContext.config.logger.error(`Schema creation failed for did: ${did} and schema: ${schema}`)
       throw new AriesFrameworkError(`Schema creation failed for did: ${did} and schema: ${schema}`)
@@ -67,12 +67,17 @@ export class PolygonLedgerService {
   public async getSchemaByDidAndSchemaId(agentContext: AgentContext, did: string, schemaId: string) {
     agentContext.config.logger.info(`Getting schema from ledger: ${did} and schemaId: ${schemaId}`)
 
-    if (!this.schemaRegistry) {
-      agentContext.config.logger.error(`Schema registry config not found`)
-      throw new AriesFrameworkError(`Schema registry not configured. Please check your configuration`)
+    const publicKeyBase58 = await this.getPublicKeyFromDid(agentContext, did)
+
+    if (!publicKeyBase58) {
+      throw new AriesFrameworkError('Public Key not found in wallet')
     }
 
-    const response = await this.schemaRegistry.getSchemaById(did, schemaId)
+    const signingKey = await this.getSigningKey(agentContext.wallet, publicKeyBase58)
+
+    const schemaRegistry = this.createSchemaRegistryInstance(signingKey)
+
+    const response = await schemaRegistry.getSchemaById(did, schemaId)
 
     if (!response) {
       agentContext.config.logger.error(`Schema not found for did: ${did} and schemaId: ${schemaId} Error: ${response}`)
@@ -82,37 +87,85 @@ export class PolygonLedgerService {
     return response
   }
 
-  public createDidRegistryInstance(privateKey: string) {
-    if (!this.rpcUrl || !this.contractAddress) {
+  public createDidRegistryInstance(signingKey: SigningKey) {
+    if (!this.rpcUrl || !this.didContractAddress) {
       throw new AriesFrameworkError('Ledger config not found')
     }
 
     return new PolygonDID({
       rpcUrl: this.rpcUrl,
-      contractAddress: this.contractAddress,
-      privateKey,
+      contractAddress: this.didContractAddress,
+      signingKey,
     })
   }
 
-  public updateSchemaRegistryInstance({
+  private createSchemaRegistryInstance(signingKey: SigningKey) {
+    if (
+      !this.rpcUrl ||
+      !this.schemaManagerContractAddress ||
+      !this.fileServerToken ||
+      !this.fileServerUrl ||
+      !this.didContractAddress
+    ) {
+      throw new AriesFrameworkError('Polygon schema module config not found')
+    }
+
+    return new PolygonSchema({
+      rpcUrl: this.rpcUrl,
+      didRegistrarContractAddress: this.didContractAddress,
+      schemaManagerContractAddress: this.schemaManagerContractAddress,
+      fileServerToken: this.fileServerToken,
+      serverUrl: this.fileServerUrl,
+      signingKey,
+    })
+  }
+
+  private async getSigningKey(wallet: Wallet, publicKeyBase58: string): Promise<SigningKey> {
+    if (!(wallet instanceof AskarWallet)) {
+      throw new AriesFrameworkError('Incorrect wallet type: Polygon Module currently only supports Askar wallet')
+    }
+
+    const keyEntry = await wallet.session.fetchKey({ name: publicKeyBase58 })
+
+    if (!keyEntry) {
+      throw new WalletError('Key not found in wallet')
+    }
+
+    const signingKey = new SigningKey(keyEntry.key.secretBytes)
+
+    keyEntry.key.handle.free()
+
+    return signingKey
+  }
+
+  private async getPublicKeyFromDid(agentContext: AgentContext, did: string) {
+    const didRepository = agentContext.dependencyManager.resolve(DidRepository)
+
+    const didRecord = await didRepository.findCreatedDid(agentContext, did)
+    if (!didRecord) {
+      throw new AriesFrameworkError('DidRecord not found')
+    }
+
+    if (!didRecord.didDocument?.verificationMethod) {
+      throw new AriesFrameworkError('VerificationMethod not found cannot get public key')
+    }
+
+    const publicKeyBase58 = didRecord.didDocument.verificationMethod[0].publicKeyBase58
+
+    return publicKeyBase58
+  }
+
+  public updateModuleConfig({
     didRegistrarContractAddress,
     fileServerToken,
-    privateKey,
     rpcUrl,
     schemaManagerContractAddress,
     serverUrl,
   }: SchemaRegistryConfig) {
-    if (!rpcUrl || !didRegistrarContractAddress || !privateKey || !fileServerToken || !schemaManagerContractAddress) {
-      throw new AriesFrameworkError('Some of the schema registry configs are not provided')
-    }
-
-    this.schemaRegistry = new PolygonSchema({
-      didRegistrarContractAddress,
-      rpcUrl,
-      fileServerToken,
-      privateKey,
-      schemaManagerContractAddress,
-      serverUrl,
-    })
+    this.rpcUrl = rpcUrl
+    this.didContractAddress = didRegistrarContractAddress
+    this.schemaManagerContractAddress = schemaManagerContractAddress
+    this.fileServerToken = fileServerToken
+    this.fileServerUrl = serverUrl
   }
 }

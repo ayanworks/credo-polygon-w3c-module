3599c4e9af3ab05ba9d4e2486cbb55301e4a7e6e
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-02-14 11:27:46 +0530

    feat: estimate fees for schema and did creation
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/PolygonApi.ts
+++ src/PolygonApi.ts
@@ -1,3 +1,5 @@
+import type { CreateSchemaOperationOptions, DidOperationOptions } from './ledger'
+
 import { AgentContext, injectable } from '@aries-framework/core'
 
 import { PolygonLedgerService } from './ledger'
@@ -20,5 +22,15 @@ export class PolygonApi {
   public async getSchemaById(did: string, schemaId: string) {
     const schemaDetails = await this.ledgerService.getSchemaByDidAndSchemaId(this.agentContext, did, schemaId)
     return schemaDetails
+  }
+
+  public async estimateFeeForDidOperation(options: DidOperationOptions) {
+    const estimatedFee = await this.ledgerService.estimateFeeForDidOperation(this.agentContext, options)
+    return estimatedFee
+  }
+
+  public async estimateFeeForSchemaOperation(options: CreateSchemaOperationOptions) {
+    const estimatedFee = await this.ledgerService.estimateFeeForSchemaOperation(this.agentContext, options)
+    return estimatedFee
   }
 }
--- src/ledger/index.ts
+++ src/ledger/index.ts
@@ -1,1 +1,1 @@
-export { PolygonLedgerService } from './PolygonLedgerService'
+export * from './PolygonLedgerService'
